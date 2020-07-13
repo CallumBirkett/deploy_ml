5290460685908e567f92000bea2e5150e89800dc
Callum Birkett <callum@callums-air.mynet>
2020-07-13 10:38:49 +0100

    Ad ml code

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -7,6 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+from apps.ml.income_classifier.random_forest import RandomForestClassifier
+import inspect
+from apps.ml.registry import MLRegistry
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +17,23 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
+
+# ML registry.
+
+
+try:
+    registry = MLRegistry()  # create ML registry
+    # rf classifier
+    rf = RandomForestClassifier()
+    # add to ML registry
+    registry.add_algorithm(endpoint_name="income_classifier",
+                           algorithm_object=rf,
+                           algorithm_name="random forest",
+                           algorithm_status="production",
+                           algorithm_version="0.0.1",
+                           owner="Callum",
+                           algorithm_description="Random forest with simple pre- and post-processing.",
+                           algorithm_code=inspect.getsource(RandomForestClassifier))
+
+except Exception as e:
+    print("Exception while loading the algorithms to the registry.", str(e))
